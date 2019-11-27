c8628e0fc019e8e2904336a258dfa8703de29b20
SergioLopezEEP <49988653+SergioLopezEEP@users.noreply.github.com>
2019-11-28 00:00:31 +0100

    First commit

---
+++ codigo.js
@@ -0,0 +1,7 @@
+//Escriba un programa que pregunte el nombre y salude a la persona
+//(Emplear el window.prompt() o solo prompt(), y el window.alert() o alert() busca información de cómo
+//funcionan) Empléalo para los ejercicios siguientes
+
+var mensaje="¿Cual es su nombre?";
+var nombre= promt(mensaje);
+alert("Hola"+ nombre);
